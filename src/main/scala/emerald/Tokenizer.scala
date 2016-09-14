1f85cc0f03e32ac53fa587fcffb6261f75458e88
Andrew R. McBurney <mcburney.andrew13@gmail.com>
2016-09-14 07:03:41 -0400

    Updated Tokenizer.

--- src/main/scala/emerald/Tokenizer.scala
+++ src/main/scala/emerald/Tokenizer.scala
@@ -5,6 +5,16 @@ import scala.io.Source
 object Tokenizer {
   def init (path :String) {
     var input :String = Source.fromFile(path).mkString
-    println (input)
+
+    // Match and create tokens recursively
+    def tokenize  (input :String, tokens :List[String]) :List[String] = {
+      input match {
+         case s if s.startsWith("html") => println("match")
+         case s if s.startsWith("doc")  => println("match 2")
+         case _ => None
+      }
+      tokens
+    }
+    tokenize (input, List())
   }
 }
