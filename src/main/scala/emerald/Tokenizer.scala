e38027224dc0fa1738b0abcfdc4ece9d474c21fe
Andrew R. McBurney <andrewrobertmcburney@gmail.com>
2016-09-14 18:48:08 -0400

    Updated Tokenizer.

--- src/main/scala/emerald/Tokenizer.scala
+++ src/main/scala/emerald/Tokenizer.scala
@@ -1,6 +1,7 @@
 package emerald
 
 import scala.io.Source
+import scala.util.control.Breaks._
 
 object Tokenizer {
   def init (path :String) {
@@ -8,13 +9,25 @@ object Tokenizer {
 
     // Match and create tokens recursively
     def tokenize  (input :String, tokens :List[String]) :List[String] = {
+      if (input.isEmpty) return tokens
+
+      print ("INPUT: " + input)
+
       input match {
-         case s if s.startsWith("html") => println("match")
-         case s if s.startsWith("doc")  => println("match 2")
-         case _ => None
+         case s if s.startsWith("html\\b") => tokenize (input.stripPrefix("html"), tokens :+ "html")
+         case s if s.startsWith("doc")  => tokenize (input.stripPrefix("doc"),  tokens :+ "doc")
+         case s if s.startsWith("head") => tokenize (input.stripPrefix("head"), tokens :+ "head")
+         case s if s.startsWith("body") => tokenize (input.stripPrefix("body"), tokens :+ "body")
+         case s if s.startsWith("\\B")    => tokenize (input.trim, tokens)
+         case _ => tokenize (input.tail, tokens)
       }
-      tokens
     }
-    tokenize (input, List())
+
+    // List of tokens from tokenize method.
+    var tok :List[String] = tokenize (input, List())
+
+    tok.foreach {
+      println
+    }
   }
 }
