f2d94b3137d892f67b46e76d9cda2bf13df375eb
Andrew R. McBurney <andrewrobertmcburney@gmail.com>
2016-09-22 08:16:13 -0400

    Updated regex for ATTR.

--- prototype/src/ruby/compiler/Tokenizer.rb
+++ prototype/src/ruby/compiler/Tokenizer.rb
@@ -10,8 +10,8 @@ class Tokenizer
   def tokenize(input, tokens = [])
     #puts "INPUT:\n#{input}\n"
     return tokens unless !input.empty?
-    if input.match(/\A(html|head|body|doctype|title|metas|scripts|styles)\b/)
-      reg = input.match(/\A(html|head|body|doctype|title|metas|scripts|styles)\b/).to_s
+    if input.match(/\A(doctype|metas|scripts|styles)\b/)
+      reg = input.match(/\A(doctype|metas|scripts|styles)\b/).to_s
 
       tokenize(
         input[reg.length..-1],
@@ -20,18 +20,11 @@ class Tokenizer
         )
       )
     elsif input.match(/\A(a|abbr|address|area|article|aside|audio|b|base|bdi|bdo|blockquote|body|br|button|canvas|caption|cite|code|col|colgroup|datalist|dd|del|details|dfn|dialog|div|dl|em|embed|fieldset|figcaption|figure|footer|form|head|header|hr|html|i|iframe|img|input|ins|kbd|keygen|label|legend|li|link|main|map|mark|menu|menuitem|meta|meter|nav|noscript|object|ol|optgroup|option|output|p|param|pre|progress|q|rp|rt|ruby|s|samp|script|section|select|small|source|span|strong|style|sub|summary|sup|table|tbody|td|textarea|tfoot|th|thead|time|title|tr|track|u|ul|var|video|wbr)\b/)
-      reg = input.match(/\A(a|abbr|address|area|article|aside|audio|b|base|bdi|bdo|blockquote|body|br|button|canvas|caption|cite|code|col|colgroup|datalist|dd|del|details|dfn|dialog|div|dl|em|embed|fieldset|figcaption|figure|footer|form|head|header|hr|html|i|iframe|img|input|ins|kbd|keygen|label|legend|li|link|main|map|mark|menu|menuitem|meta|meter|nav|noscript|object|ol|optgroup|option|output|p|param|pre|progress|q|rp|rt|ruby|s|samp|script|section|select|small|source|span|strong|style|sub|summary|sup|table|tbody|td|textarea|tfoot|th|thead|time|title|tr|track|u|ul|var|video|wbr)\b/).to_s
+      reg = input.match(/\A(a|abbr|address|area|article|aside|audio|b|base|bdi|bdo|blockquote|body|br|button|canvas|caption|cite|code|col|colgroup|datalist|dd|del|details|dfn|dialog|div|dl|em|embed|fieldset|figcaption|figure|footer|form|head|header|hr|html|h[1-6]|i|iframe|img|input|ins|kbd|keygen|label|legend|li|link|main|map|mark|menu|menuitem|meta|meter|nav|noscript|object|ol|optgroup|option|output|p|param|pre|progress|q|rp|rt|ruby|s|samp|script|section|select|small|source|span|strong|style|sub|summary|sup|table|tbody|td|textarea|tfoot|th|thead|time|title|tr|track|u|ul|var|video|wbr)\b/).to_s
 
       tokenize(
         input[reg.length..-1],
         tokens.push(Token.new(reg, 'TAG'))
-      )
-    elsif input.match(/\Ah[1-6]\b/)
-      reg = input.match(/\Ah[1-6]\b/).to_s
-
-      tokenize(
-        input[reg.length..-1],
-        tokens.push(Token.new(reg, 'HEADER'))
       )
     elsif input.match(/\A(\(|\)|"|'|\>|\<|\@|=|!|\?|#|,|\.|\+|-|\$|\/)/)
       reg = input.match(/\A(\(|\)|"|'|\>|\<|\@|=|!|\?|#|,|\.|\+|-|\$|\/)/).to_s
@@ -40,16 +33,16 @@ class Tokenizer
         input[reg.length..-1],
         tokens.push(Token.new(reg, 'SYMBOL'))
       )
-    elsif input.match(/\A(href|rel)\b/)
-      reg = input.match(/\A(href|rel)\b/).to_s
+    elsif input.match(/\A(accept|accept-charset|accesskey|action|alt|async|autocomplete|autofocus|autoplay|challenge|charset|checked|cite|class|cols|colspan|content|contenteditable|contextmenu|controls|coords|data|data-(.+?)|datetime|default|defer|dir|dirname|disabled|download|draggable|dropzone|enctype|for|form|formaction|headers|height|hidden|high|href|hreflang|http-equiv|id|ismap|keytype|kind|label|lang|list|loop|low|manifest|max|maxlength|media|method|min|multiple|muted|name|novalidate|open|optimum|pattern|placeholder|poster|preload|readonly|rel|required|reversed|rows|rowspan|sandbox|scope|scoped|selected|shape|size|sizes|span|spellcheck|src|srcdoc|srclang|start|step|style|tabindex|target|title|translate|type|usemap|value|width|wrap)\b/)
+    # Have set of valid attributes for each tag when checking for errors
+      reg = input.match(/\A(accept|accept-charset|accesskey|action|alt|async|autocomplete|autofocus|autoplay|challenge|charset|checked|cite|class|cols|colspan|content|contenteditable|contextmenu|controls|coords|data|data-(.+?)|datetime|default|defer|dir|dirname|disabled|download|draggable|dropzone|enctype|for|form|formaction|headers|height|hidden|high|href|hreflang|http-equiv|id|ismap|keytype|kind|label|lang|list|loop|low|manifest|max|maxlength|media|method|min|multiple|muted|name|novalidate|open|optimum|pattern|placeholder|poster|preload|readonly|rel|required|reversed|rows|rowspan|sandbox|scope|scoped|selected|shape|size|sizes|span|spellcheck|src|srcdoc|srclang|start|step|style|tabindex|target|title|translate|type|usemap|value|width|wrap)\b/).to_s
 
       tokenize(
         input[reg.length..-1],
         tokens.push(Token.new(reg, 'ATTR'))
       )
-    elsif input.match(/\Arel\b|\Arel=/)
-      tokenize(input[3..-1], tokens.push(Token.new('rel', 'ATTR')))
     elsif input.match(/\A(click|hover)\b/)
+      # onabort|onafterprint|onbeforeprint|onbeforeunload|
       reg = input.match(/\A(click|hover)\b/).to_s
 
       tokenize(
