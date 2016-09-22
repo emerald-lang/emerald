ed0b5672f267ca942b3fd2b686380af065a4eb40
Andrew R. McBurney <andrewrobertmcburney@gmail.com>
2016-09-21 20:22:33 -0400

    Updated tokenizer regex matches.

--- prototype/src/ruby/compiler/Tokenizer.rb
+++ prototype/src/ruby/compiler/Tokenizer.rb
@@ -10,40 +10,35 @@ class Tokenizer
   def tokenize(input, tokens = [])
     #puts "INPUT:\n#{input}\n"
     return tokens unless !input.empty?
-    if input.match(/\Ahtml\b/)
-      tokenize(input[4..-1], tokens.push(Token.new('html', 'KEYWORD')))
-    elsif input.match(/\Ahead\b/)
-      tokenize(input[4..-1], tokens.push(Token.new('head', 'KEYWORD')))
-    elsif input.match(/\Abody\b/)
-      tokenize(input[4..-1], tokens.push(Token.new('body', 'KEYWORD')))
-    elsif input.match(/\Adoc\b/)
-      tokenize(input[3..-1], tokens.push(Token.new('doc', 'KEYWORD')))
-    elsif input.match(/\Ahref\b/)
-      tokenize(input[4..-1], tokens.push(Token.new('href', 'KEYWORD')))
-    elsif input.match(/\Atitle\b/)
-      tokenize(input[5..-1], tokens.push(Token.new('title', 'KEYWORD')))
-    elsif input.match(/\Ameta\b/)
-      tokenize(input[4..-1], tokens.push(Token.new('meta', 'KEYWORD')))
-    elsif input.match(/\Ascript\b/)
-      tokenize(input[6..-1], tokens.push(Token.new('script', 'KEYWORD')))
-    elsif input.match(/\Adiv\b/)
-      tokenize(input[3..-1], tokens.push(Token.new('div', 'TAG')))
-    elsif input.match(/\Ah1\b/)
-      tokenize(input[2..-1], tokens.push(Token.new('h1', 'HEADER')))
-    elsif input.match(/\Ah2\b/)
-      tokenize(input[2..-1], tokens.push(Token.new('h2', 'HEADER')))
-    elsif input.match(/\Ah3\b/)
-      tokenize(input[2..-1], tokens.push(Token.new('h3', 'HEADER')))
-    elsif input.match(/\Ah4\b/)
-      tokenize(input[2..-1], tokens.push(Token.new('h4', 'HEADER')))
-    elsif input.match(/\Ah5\b/)
-      tokenize(input[2..-1], tokens.push(Token.new('h5', 'HEADER')))
-    elsif input.match(/\Ah6\b/)
-      tokenize(input[2..-1], tokens.push(Token.new('h6', 'HEADER')))
+    if input.match(/\A(html|head|body|doc|href|title|metas|scripts|styles)\b/)
+      reg = input.match(/\A(html|head|body|doc|href|title|metas|scripts|styles)\b/).to_s
+
+      tokenize(
+        input[reg.length..-1],
+        tokens.push(
+          Token.new(reg, 'KEYWORD')
+        )
+      )
+    elsif input.match(/\A(div|section|article|figure|figcaption)\b/)
+      reg = input.match(/\A(div|section|article|figure|figcaption)\b/).to_s
+
+      tokenize(
+        input[reg.length..-1],
+        tokens.push(Token.new(reg, 'TAG'))
+      )
+    elsif input.match(/\Ah[1-6]\b/)
+      reg = input.match(/\Ah[1-6]\b/).to_s
+
+      tokenize(
+        input[reg.length..-1],
+        tokens.push(Token.new(reg, 'HEADER'))
+      )
     elsif input.match(/\A\(/)
       tokenize(input[1..-1], tokens.push(Token.new('(', 'LPAREN')))
     elsif input.match(/\A\)/)
       tokenize(input[1..-1], tokens.push(Token.new(')', 'RPAREN')))
+    elsif input.match(/\A"/)
+      tokenize(input[1..-1], tokens.push(Token.new('"', 'QUOTE')))
     elsif input.match(/\A'/)
       tokenize(input[1..-1], tokens.push(Token.new('\'', 'QUOTE')))
     elsif input.match(/\A\>/)
@@ -54,6 +49,8 @@ class Tokenizer
       tokenize(input[1..-1], tokens.push(Token.new('@', 'AT')))
     elsif input.match(/\A=/)
       tokenize(input[1..-1], tokens.push(Token.new('=', 'EQUALS')))
+    elsif input.match(/\A\!/)
+      tokenize(input[1..-1], tokens.push(Token.new('!', 'SYMBOL')))
     elsif input.match(/\A\?/)
       tokenize(input[1..-1], tokens.push(Token.new('?', 'QUESTION')))
     elsif input.match(/\A#/)
@@ -75,20 +72,17 @@ class Tokenizer
     elsif input.match(/\Arel\b|\Arel=/)
       tokenize(input[3..-1], tokens.push(Token.new('rel', 'ATTR')))
     elsif input.match(/\A(click|hover)\b/)
-      tokenize(input[5..-1], tokens.push(Token.new(input.match(/\A(click|hover)\b/).to_s, 'EVENT')))
-    elsif input.start_with? "\""
-      i = input[1..-1].index("\"")
-      if i.nil?
-        puts "String error on line: #.\nNo closing quotation."
-      else
-        tokenize(input[(i + 2)..-1], tokens.push(Token.new(input[0..i + 1], 'STRING')))
-      end
+      reg = input.match(/\A(click|hover)\b/).to_s
+
+      tokenize(
+        input[reg.length..-1],
+        tokens.push(Token.new(reg, 'EVENT'))
+      )
     elsif input.start_with? "\n"
       tokenize(input[1..-1], tokens.push(Token.new("\n", 'NEWLINE')))
     elsif input.match(/\A(\w+)\b/)
-      var = input.match(/\A(\w+)\b/).to_s
-      len = var.length
-      tokenize(input[len..-1], tokens.push(Token.new(var, 'VAR')))
+      reg = input.match(/\A(\w+)\b/).to_s
+      tokenize(input[reg.length..-1], tokens.push(Token.new(reg, 'VAR')))
     elsif input.start_with? ' '
       tokenize(input[1..-1], tokens.push(Token.new(' ', 'SPACE')))
     else
