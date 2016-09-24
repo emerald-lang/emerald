330568bfb731de09fd6796a8bc20d7f8227a0894
Andrew R. McBurney <andrewrobertmcburney@gmail.com>
2016-09-24 13:36:41 -0400

    Updated semantic analyzer.

--- prototype/src/ruby/compiler/Analyzer.rb
+++ prototype/src/ruby/compiler/Analyzer.rb
@@ -2,18 +2,44 @@
 # from token stream
 
 require 'singleton'
+require_relative 'Node'
 
 class Analyzer
   include Singleton
 
-  def make_tree(tokens, level = 0, flag = false)
-    i = tokens.shift
+  def make_tree(tokens, nodes, level = 0, parent = nil, flag = false)
+    i = Node.new(tokens.shift, parent)
+
     return unless !i.nil?
     if i.type == "NEWLINE"
-      make_tree(tokens, 0, true)
+      make_tree(
+        tokens,
+        nodes.push(i),
+        level,
+        parent,
+        true
+      )
     elsif i.type == "SPACE" and flag
-      puts i.value.length
+      puts "NEW LINE: #{level}"
+
+      if i.value.length < level
+        puts "no more children"
+      end
+
+      make_tree(
+        tokens,
+        nodes.push(i),
+        i.value.length,
+        parent
+      )
+    else
+      puts "LEVEL: #{level}"
+      make_tree(
+        tokens,
+        nodes.push(i),
+        level,
+        parent
+      )
     end
-    make_tree(tokens, 0)
   end
 end
--- prototype/src/ruby/compiler/Node.rb
+++ prototype/src/ruby/compiler/Node.rb
@@ -1,12 +1,18 @@
 # Node for syntax tree
 
+require_relative 'Token'
+
 class Node
-  def initialize(type)
-    @type = type
-    @children = Set.new
+  def initialize(token, parent)
+    @type = token.type
+    @value = token.value
+    @children = Array.new
+    @parent = parent
   end
 
   def addChild(child)
     @children.push(child)
   end
+
+  attr_reader :type, :value
 end
