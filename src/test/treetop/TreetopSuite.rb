fc0153590ee27d04bd7141c236514673b8ebf10b
Andrew R. McBurney <andrewrobertmcburney@gmail.com>
2016-09-30 07:49:58 -0400

    Updated test suite and removed folders.

--- src/test/treetop/TreetopSuite.rb
+++ src/test/treetop/TreetopSuite.rb
@@ -7,22 +7,33 @@ Treetop.load "../../ruby/treetop/grammar/emerald"
 
 class TreetopSuite < Test::Unit::TestCase
 
-  def test_samples
-    parser = EmeraldParser.new
-    path = "samples/emerald/tests/valid/test/"
+  @@parser = EmeraldParser.new
 
+  def walk(path)
     Dir.foreach(path) do |file|
-      next if file == ".." or file == "."
-      f = File.open(path + file).read
-      assert_not_equal(parser.parse(f), nil)
+      new_path = File.join(path, file)
+
+      if file == ".." or file == "."
+        next
+      elsif File.directory?(new_path)
+        walk(new_path)
+      else
+        f = File.open(path + "/" + file)
+        assert_not_equal(@@parser.parse(f.read), nil)
+      end
     end
+  end
+
+  def test_samples
+    path = "samples/emerald/tests/valid/"
+    walk(path)
 
     invalid = "samples/emerald/tests/invalid/"
 
     Dir.foreach(invalid) do |file|
       next if file == ".." or file == "."
       f = File.open(invalid + file).read
-      assert_equal(parser.parse(f), nil)
+      assert_equal(@@parser.parse(f), nil)
     end
   end
 
