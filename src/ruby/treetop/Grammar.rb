454be9cf94c349f4d7e45fda16c167e6e162df16
Andrew R. McBurney <andrewrobertmcburney@gmail.com>
2016-10-03 17:41:26 -0400

    Updated grammar.

--- src/ruby/treetop/Grammar.rb
+++ src/ruby/treetop/Grammar.rb
@@ -4,27 +4,27 @@ require 'treetop'
 Treetop.load 'grammar/tokens'
 Treetop.load 'grammar/emerald'
 
+# Class
 class Grammer
   @@parser = EmeraldParser.new
 
-  text = File.open('../../test/treetop/samples/emerald/tests/valid/text/1.emr').read
-  text2 = File.open('../../test/treetop/samples/emerald/tests/valid/text/2.emr').read
-  text3 = File.open('../../test/treetop/samples/emerald/tests/valid/text/3.emr').read
+  text = File.open('../../test/treetop/samples/emerald/tests/valid/nested.emr').read
 
   tests = [
-    text.chomp,
-    text2.chomp,
-    text3.chomp
+    text.chomp
   ]
 
   tests.each do |test|
     parsed = @@parser.parse(test)
 
     if parsed.nil?
-      puts 'Failed: #{test}'
+      puts "Failed: #{test}"
       puts @@parser.failure_reason
     else
-      puts 'Passed #{test}'
+      puts "Passed #{test}"
+      parsed.each do |t|
+        puts t
+      end
     end
   end
 end
