0994676b0e520e41274c07e663d388c0caa0a4f2
Andrew R. McBurney <andrewrobertmcburney@gmail.com>
2016-10-16 10:30:56 -0400

    Updated grammar nested rule.

--- src/ruby/treetop/nodes/Nested.rb
+++ src/ruby/treetop/nodes/Nested.rb
@@ -8,24 +8,29 @@ class Nested < Node
     #log("NESTED")
     indent = 0 if indent.nil?
     elem = elements if elem.nil?
+    temp = false
 
     elem.map do |e|
       if e.is_a?(TagStatement)
-        puts "#{e.to_html()}"
+        inner = 0
+
+        puts "#{e.to_html()} [#{indent}]"
         x = to_html(elem[1..-1], indent)
-        puts "</#{e.elements[0].text_value}>"
+        puts "</#{e.elements[0].text_value}> [#{indent}]"
 
-        if x.flatten.compact.any?
-          puts 'fuck'
-          to_html(elem[1..-1])
+        inner = x.flatten.compact.first if x.flatten.compact.any?
+        p inner
+
+        if inner == indent
+          p elem
+          to_html(elem[1..-1], indent)
+        else
+          break inner
         end
-
-        break
       elsif e.is_a?(IndentNested)
         if @@isTrue.(e.elements[0], indent)
           indent = e.elements[0].elements.length * 2
-          return elem
-          # call to_html on remaining stuff.
+          return indent
         else
           indent = e.elements[0].elements.length * 2
           e.elements[1].to_html(nil, indent)
@@ -33,8 +38,7 @@ class Nested < Node
       elsif e.is_a?(IndentLine)
         if @@isTrue.(e.elements[0], indent)
           indent = e.elements[0].elements.length * 2
-          return elem
-          # call to_html on remaining stuff.
+          return indent
         else
           indent = e.elements[0].elements.length * 2
           e.to_html()
