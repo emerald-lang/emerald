0fc534239b726edcd8db7fdc1f4a8cd2ed8d21c0
Andrew R. McBurney <andrewrobertmcburney@gmail.com>
2016-10-24 09:05:16 -0400

    Updated PreProcessor.rb.
    Cleaned up PreProcessor.rb code.

--- src/ruby/treetop/PreProcessor.rb
+++ src/ruby/treetop/PreProcessor.rb
@@ -5,10 +5,28 @@
 # by a context free grammar.
 #
 class PreProcessor
-  # pass once, remove any whitespace that is not on a line with text.
-  # pass second time, add {} for nesting.
-  # pass input to Grammar file to be parsed by a CFG.
-  f = File.open("sample.emr")
+  input = File.open("sample.emr", "r").read
 
-  puts f.read
+  current_indent = 0; new_indent = 0; b_count = 0
+
+  input.each_line do |line|
+    # remove any newlines for intermediate form
+    next if line.lstrip.length == 0
+
+    new_indent = line.length - line.lstrip.length
+
+    if new_indent > current_indent
+      puts "{"; b_count += 1
+      current_indent = new_indent
+    elsif new_indent < current_indent && new_indent != 0
+      puts "}"; b_count -= 1
+    end
+
+    puts line
+  end
+
+  # Print out remaining braces.
+  for i in 1..b_count do
+    puts "}"
+  end
 end
