a45a8b9abf83072fcbbd0d411d8a260a8c901d5b
Andrew R. McBurney <andrewrobertmcburney@gmail.com>
2016-11-04 17:41:45 -0400

    Fixed TagStatement.rb rule.

--- src/ruby/nodes/Nested.rb
+++ src/ruby/nodes/Nested.rb
@@ -9,7 +9,7 @@ require_relative 'Node'
 #
 class Nested < Node
   def to_html
-    elements[0].to_html +
+    elements[0].to_html_tag +
       elements[4].to_html +
       "</#{elements[0].elements[0].text_value}>"
   end
--- src/ruby/nodes/TagStatement.rb
+++ src/ruby/nodes/TagStatement.rb
@@ -5,18 +5,14 @@ require_relative 'Node'
 
 class TagStatement < Node
   def to_html
+    "<#{elements[0].text_value}>#{elements[2].text_value}</#{elements[0].text_value}>"
+  end
 
-    # this now closes off nested tag statements prematurely - need to fix
-    return "<#{elements[0].text_value}>#{elements[2].text_value}</#{elements[0].text_value}>"
-
-    # todo: make this work
-    if elements[4].is_a?(AttributeList)
-      output += ("<#{elements[0].text_value}#{elements[4].to_html()}>" +
-                 "#{elements[2].text_value.delete! '(),'}")
-    elsif !elements[2].empty?
-      output += "<#{elements[0].text_value} #{elements[2].to_html("")}>"
+  def to_html_tag
+    if !elements[2].empty?
+      "<#{elements[0].text_value} #{elements[2].to_html("")}>"
     else
-      output += "<#{elements[0].text_value}>"
+      "<#{elements[0].text_value}>"
     end
   end
 end
