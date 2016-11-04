1e7f1771f04247fd03e7653bbfae1595f68142f5
Andrew R. McBurney <andrewrobertmcburney@gmail.com>
2016-11-04 08:36:00 -0400

    Updated testing files.

--- src/test/PreProcessorSuite.rb
+++ src/test/PreProcessorSuite.rb
@@ -2,7 +2,7 @@
 
 require 'test/unit'
 
-require_relative '../../ruby/PreProcessor'
+require_relative '../ruby/PreProcessor'
 
 #
 # Unit testing for Emerald PreProcessor. Asserts that valid emerald is
@@ -10,7 +10,32 @@ require_relative '../../ruby/PreProcessor'
 # representation.
 #
 class PreProcessorSuite < Test::Unit::TestCase
+  include PreProcessor
+
+  # Recursively walk directory and get all test case files for the test suite.
+  # Performs preprocessing operation on each file and stores the results in a
+  # list.
+  def walk(path, list = [])
+    Dir.foreach(path) do |file|
+      new_path = File.join(path, file)
+
+      next if file == '..' || file == '.'
+
+      if File.directory?(new_path)
+        walk(new_path, list)
+      else
+        f = File.open(path + '/' + file)
+        list.push([PreProcessor.process_emerald(), path + '/' + file])
+      end
+    end
+
+    list
+  end
+
+  # We only test the valid samlpes, because the invalid emerald samples will
+  # also be preprocessed regardless of their semantics. Error checking happens
+  # in the parsing and code generation phase.
   def test_valid_output
-    assert_equal(true, true)
+    output = walk('samples/emerald/tests/valid')
   end
 end
