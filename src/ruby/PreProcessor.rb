f93aa424ffed179f7d54ff6d63517232e2800097
Andrew R. McBurney <andrewrobertmcburney@gmail.com>
2016-11-07 08:03:54 -0500

    Made PreProcessor.rb more rubocop compliant.

--- src/ruby/PreProcessor.rb
+++ src/ruby/PreProcessor.rb
@@ -8,59 +8,65 @@ require_relative 'Grammar'
 # indentation.
 #
 module PreProcessor
-  def self.process_emerald(file_name)
-    input = File.open(file_name, "r").read
+  @in_literal = false
+  @current_indent = 0
+  @new_indent = 0
+  @b_count = 0
+  @output = ''
 
-    in_literal = false;
-    current_indent = 0; new_indent = 0; b_count = 0; output = ''
+  def self.process_emerald(file_name)
+    input = File.open(file_name, 'r').read
 
     input.each_line do |line|
       # remove any blank lines for intermediate form
-      next if line.lstrip.length == 0
+      next if line.lstrip.empty?
 
       new_indent = line.length - line.lstrip.length
 
-      if new_indent > current_indent
-        unless in_literal
-          output += "{\n"; b_count += 1
-          current_indent = new_indent
+      if new_indent > @current_indent
+        unless @in_literal
+          @output += "{\n"
+          @b_count += 1
+          @current_indent = new_indent
         end
-      elsif new_indent < current_indent && new_indent != 0
-        if in_literal
-          output += "$\n"
-          in_literal = false
-          for i in 2..((current_indent - new_indent) / 2) do
-            output += "}\n"; b_count -= 1
+      elsif new_indent < @current_indent && new_indent.nonzero?
+        if @in_literal
+          @output += "$\n"
+          @in_literal = false
+          for i in 2..((@current_indent - new_indent) / 2) do
+            @output += "}\n"
+            @b_count -= 1
           end
         else
-          for i in 1..((current_indent - new_indent) / 2) do
-            output += "}\n"; b_count -= 1
+          for i in 1..((@current_indent - new_indent) / 2) do
+            @output += "}\n"
+            @b_count -= 1
           end
         end
-        current_indent = new_indent
+        @current_indent = new_indent
       end
 
-      if in_literal
+      if @in_literal
         # Crop off only Emerald indent whitespace to preserve
         # whitespace in the literal
-        output += (line[current_indent..-1] || "").gsub('$', '\$')
+        @output += (line[@current_indent..-1] || '').gsub('$', '\$')
         # $ is our end character, so we need to escape it in
         # the literal
       else
-        output += line.lstrip
+        @output += line.lstrip
       end
 
-      if line.rstrip.end_with?("->")
-        in_literal = true
-        current_indent += 2
+      if line.rstrip.end_with?('->')
+        @in_literal = true
+        @current_indent += 2
       end
     end
 
     # Print out remaining braces.
-    for i in 1..b_count do
-      output += "}\n"
+    for i in 1..@b_count do
+      @output += "}\n"
     end
 
-    output
+    @output
   end
 end
