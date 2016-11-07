03f7c4010ce535c79623aed4240acfdce4029e71
Andrew R. McBurney <andrewrobertmcburney@gmail.com>
2016-11-07 17:22:52 -0500

    Made Main.rb more rubocop compliant.

--- src/ruby/Main.rb
+++ src/ruby/Main.rb
@@ -3,7 +3,6 @@
 require_relative 'Grammar'
 require_relative 'PreProcessor'
 
-
 # TODO: add command line arguments.
 # - b --beautify (format the html - minified by default)
 # - p --source_path (source destination to write file to)
@@ -11,10 +10,10 @@ require_relative 'PreProcessor'
 # Parses a context free grammar from the preprocessed emerald and generates
 # html associated with corresponding abstract syntax tree.
 class Main
-  unless ARGV.length == 0
+  unless ARGV.length.zero?
     file_name = ARGV[0]
   else
-    file_name = "index.emr"
+    file_name = 'index.emr'
   end
 
   preprocessed_emerald = PreProcessor.process_emerald(file_name)
@@ -22,7 +21,7 @@ class Main
   out = abstract_syntax_tree.to_html
 
   puts out
-  File.open(file_name + '.html', "w") do |file|
+  File.open(file_name + '.html', 'w') do |file|
     file.write(out)
   end
 end
