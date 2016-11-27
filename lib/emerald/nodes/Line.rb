8faf57e96a8eff7eb2759ca2db79a75f1920db5b
Dave Pagurek <dave@davepagurek.com>
2016-11-27 15:31:53 -0800

    New comment multiline (#32)
    * Add new comment style to grammar
    
    * Fix multiline comment support

--- lib/emerald/nodes/Line.rb
+++ lib/emerald/nodes/Line.rb
@@ -9,10 +9,6 @@ class Line < Node
   def to_html(context)
     e = elements[0]
 
-    unless e.is_a?(TagStatement) || e.is_a?(Text)
-      raise "well you shouldn't be here :("
-    end
-
     e.to_html(context)
   end
 end
