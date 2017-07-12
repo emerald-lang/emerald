f42e209768c28b5c11d1beafa05bd60d6d281b95
Andrew R. McBurney <andrewrobertmcburney@gmail.com>
2017-07-11 23:58:18 -0700

    Updated templating tests.

--- cpp/test/templating_tests.cpp
+++ cpp/test/templating_tests.cpp
@@ -24,7 +24,7 @@ TEST_CASE("templating", "[grammar]") {
       "<section id=\"header\" class=\"class-prefix-text\" height=\"50px\" width=\"200px\">"
       "Attributes follow parentheses.</section>";
 
-    REQUIRE(TestHelper::convert(input, {}) == output);
+    REQUIRE(TestHelper::convert(input, { "prefix", "class-prefix" }) == output);
   }
 
   SECTION("works with simple templating") {
@@ -47,13 +47,91 @@ TEST_CASE("templating", "[grammar]") {
 
   SECTION("works in multiline literals") {
     std::vector<std::string> input = {
-      "pre ->"
+      "pre ->",
       "  Hello, world,",
       "  my name is |name|"
     };
-    const char *output = "<h1>Hello, Dave</h1>";
+    const std::string output = "<pre>Hello, world, my name is Dave</pre>";
 
     REQUIRE(TestHelper::convert(input, { "name", "Dave" }) == output);
   }
+
+  SECTION("does not template in multiline templateless literals") {
+    std::vector<std::string> input = {
+      "h1 =>",
+      "  Hello, world,",
+      "  my name is |name|"
+    };
+    const std::string output = "<pre>Hello, world, my name is |name|</pre>";
+
+    REQUIRE(TestHelper::convert(input, { "name", "Dave" }) == output);
+  }
+
+  SECTION("escaping") {
+
+    SECTION("handles escaped pipes") {
+      std::vector<std::string> input = {
+        "h1 Hello, \\||name|"
+      };
+      const char *output = "<h1>Hello, |Andrew</h1>";
+
+      REQUIRE(TestHelper::convert(input, { "name", "Andrew" }) == output);
+    }
+
+    SECTION("handles escaped escaped pipes") {
+      std::vector<std::string> input = {
+        "h1 Hello, \\\\||name|"
+      };
+      const char *output = "<h1>Hello, \Dave</h1>";
+
+      REQUIRE(TestHelper::convert(input, { "name", "Dave" }) == output);
+    }
+
+    SECTION("handles braces in multiline literals") {
+      std::vector<std::string> input = {
+        "h1 =>",
+        "  hey look a brace",
+        "  }"
+      };
+      const char *output = "<h1>hey look a brace }</h1>";
+
+      REQUIRE(TestHelper::convert(input, {}) == output);
+    }
+
+    SECTION("handles braces in inline literals") {
+      std::vector<std::string> input = {
+        "h1 hey look a brace }"
+      };
+      const char *output = "<h1>hey look a brace }</h1>";
+
+      REQUIRE(TestHelper::convert(input, {}) == output);
+    }
+
+    SECTION("does not template templateless literals") {
+      std::vector<std::string> input = {
+        "h1 =>",
+        "  if (a || b) {",
+        "    console.log(\"truthy\\n\");",
+        "  }"
+      };
+      const char *output =
+        "<h1>if (a || b) {"
+        "  console.log(&quot;truthy\\n&quot;);"
+        "}</h1>";
+
+      REQUIRE(TestHelper::convert(input, {}) == TestHelper::whitespace_agnostic(output));
+    }
+
+    SECTION("escaping parentheses works") {
+      std::vector<std::string> input = {
+        "section here's some text \\(and some stuff in brackets\\)"
+      };
+      const char *output =
+        "<section class=\"something\">here\'s some text (and some stuff in brackets)</section>";
+
+      REQUIRE(TestHelper::convert(input, {}) == output);
+    }
+
+  } // SECTION("escaping")
 
 } // TEST_CASE("templating", "[grammar]")
